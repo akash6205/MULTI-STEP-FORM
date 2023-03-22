40b3bcb2e35c652eb0268d56f11769055036c788
thakur1203 <wtf.adii99@gmail.com>
2023-03-23 00:44:31 +0530

    step-2 mobile design completed

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const number = document.querySelector('#number')
 const stepCount = document.querySelectorAll('.step')
 const button = document.querySelector('#button')
 const form = document.querySelector('#form')
+const title = document.querySelector('.title')
+const subtitle = document.querySelector('.sub-title')
+const goBack = document.querySelector('.go-back')
 
-
-// console.log(formDate.get('inputName'));
 
 function changingStep(){
         let active = document.querySelector('.active')
@@ -19,17 +20,26 @@ function velidation (type){
         type.nextElementSibling.style.display = 'none'
         type.style.border = '1px solid blue';
 }
+
 function velidationError (type){
         type.nextElementSibling.style.display = 'block'
         type.style.border = '1px solid var(--clr-primary-red-100)';
 }
-let n = 0;
+
+function chnageTitle(){
+    title.innerText = 'Select your plan'
+    subtitle.innerText = 'You have the option of monthly or yearly billing'
+    goBack.parentElement.style.justifyContent = 'space-between'
+    goBack.style.display = 'block'
+}
+
 button.addEventListener('click' ,()=>{
     let newFormData = new FormData(form)
         if(newFormData.get('name') != '' && newFormData.get('email') != '' && newFormData.get('number') != ''){
             changingStep()
-            velidation(email)
+            chnageTitle()
             velidation(inputName)
+            velidation(email)
             velidation(number)
         }
     else{
